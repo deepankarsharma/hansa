0fe9a4103a0e1b3d1b11f5bfd56f839e9b1946f8
Deepankar Sharma <deepankarsharma@gmail.com>
2025-03-04 10:09:01 -0500

    Format 001-vector-add.c

--- kernels/001-vector-add.c
+++ kernels/001-vector-add.c
@@ -1,6 +1,5 @@
-__attribute__((visibility("default"), amdgpu_kernel))
-void add_arrays(
-    int* input_a, int* input_b, int* output) {
+__attribute__((visibility("default"), amdgpu_kernel)) void
+add_arrays(int* input_a, int* input_b, int* output) {
   int index =
       __builtin_amdgcn_workgroup_id_x() * __builtin_amdgcn_workgroup_size_x() +
       __builtin_amdgcn_workitem_id_x();
